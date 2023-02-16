faa4ddcda02c191ef36832b84ce4b3a376ad1bdd
brian-ambani <brainambani1@gmail.com>
2023-02-16 15:50:05 -0800

    handled commandline arguements

--- main.c
+++ main.c
@@ -6,15 +6,25 @@ int main(int argc, char **argv)
 	size_t n = 0;
 	char *s_token;
 	pid_t my_pid;
-	int status;
+	int status, i = 0;
 
 	while(1)
 	{
 		printf("(shell)$ ");
 		getline(&command, &n, stdin);
-		s_token = strtok(command, "\n");
+		s_token = strtok(command, " \n");
 
-		char *arr[] = {s_token, NULL};
+		char **arr = malloc(sizeof(char *) * 32);
+		arr[0] = s_token;
+
+
+		while(s_token != NULL)
+		{
+			s_token = strtok(NULL, " \n");
+			arr[i] = s_token;
+			i++;
+		}
+
 
 		my_pid = fork();
 		
